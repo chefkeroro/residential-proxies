4558dc6ff6b020f0418a272cce93755330a7df14
Justas Bagdonavicius <56535646+oxyjustas@users.noreply.github.com>
2020-02-28 13:50:18 +0200

    Create ruby.rb

---
+++ Ruby/ruby.rb
@@ -0,0 +1,13 @@
+require 'uri'
+require 'net/http'
+
+uri = URI.parse('https://ipinfo.io/')
+proxy = Net::HTTP::Proxy('pr.oxylabs.io', 7777, 'customer-USERNAME-cc-US', 'PASSWORD')
+
+req = Net::HTTP::Get.new(uri.path)
+
+result = proxy.start(uri.host,uri.port) do |http|
+    http.request(req)
+end
+
+puts result.body
